b38138b88319584e7bf10ec6cb8773f8c72e2051
Code-the-Code <saravanan.b3201@gmail.com>
2023-07-03 17:01:42 +0530

    exercise 1.7

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -17,24 +17,40 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const goodClick = () => {
     const updatedGood = good + 1
+    const updateTotal = total + 1
+    const updateAvg = average + 1
+    const updatePositive = positive + 1
     setGood(updatedGood)
+    setTotal(updateTotal)
+    setAverage(updateAvg)
+    setPositive(updatePositive)
     console.log({updatedGood})
   }
 
   const neutralClick = () => {
     const updatedNeutral = neutral + 1
+    const updateTotal = total + 1
     setNeutral(updatedNeutral)
+    setTotal(updateTotal)
     console.log({updatedNeutral})
   }
 
   const badClick = () => {
     const updatedBad = bad + 1
+    const updateTotal = total + 1
+    const updateAvg = average - 1
     setBad(updatedBad)
+    setTotal(updateTotal)
+    setAverage(updateAvg)
     console.log({updatedBad})
   }
+
 
   return (
     <div>
@@ -49,6 +65,9 @@ function App() {
       <Display name={'Good'} count={good}/>
       <Display name={'Neutral'} count={neutral}/>
       <Display name={'Bad'} count={bad}/>
+      <Display name={'All'} count={total}/>
+      <Display name={'Average'} count={average/total}/>
+      <Display name={'positive'} count={(positive/total)*100+' %'}/>
     </div>
   );
 }
